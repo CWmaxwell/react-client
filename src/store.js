033dd25e4792b8bd8ecd951b91ed44968fd3b6da
陈文 <chenwen@chenwendeMacBook-Pro.local>
2019-05-14 00:32:04 +0800

    fix bug

--- src/store.js
+++ src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 const middleware = [thunk];
 const initialState = {};
+
 const store = createStore(
     rootReducer, 
     initialState, 
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__&&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__&&
+        // window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
